8f482205123efbb50fc2c939fe1ce3a860da4119
greyxp1 <greyxp999@gmail.com>
2026-05-21 16:09:26 -0400

    replaced icon theme packages with gtk theming, removed system level catppucin theming and allowed overwriting home manager backup file

--- modules/nixos/home.nix
+++ modules/nixos/home.nix
@@ -11,42 +11,49 @@
       services.flatpak.enable = true;
       programs.dconf.enable = true;
 
-      catppuccin = {
-        enable = true;
-        flavor = "mocha";
-        accent = "mauve";
-        cache.enable = true;
-      };
-
       home-manager = {
         useGlobalPkgs = true;
         useUserPackages = true;
         backupFileExtension = "backup";
+        overwriteBackup = true;
         sharedModules = [ inputs.catppuccin.homeModules.catppuccin ];
+
         users.grey =
-          { pkgs, ... }:
+          { pkgs, lib, ... }:
           {
             home = {
               username = "grey";
               homeDirectory = "/home/grey";
               stateVersion = "26.05";
+              packages = [ inputs.helium.packages.${pkgs.stdenv.hostPlatform.system}.default ];
+
               pointerCursor = {
                 package = pkgs.catppuccin-cursors.mochaMauve;
                 name = "catppuccin-mocha-mauve-cursors";
                 size = 24;
                 gtk.enable = true;
               };
-              packages = with pkgs; [
-                adwaita-icon-theme
-                hicolor-icon-theme
-                inputs.helium.packages.${pkgs.stdenv.hostPlatform.system}.default
-              ];
             };
 
             catppuccin = {
               enable = true;
               flavor = "mocha";
               accent = "mauve";
+            };
+
+            gtk = {
+              enable = true;
+              theme = {
+                name = "catppuccin-mocha-mauve-standard";
+                package = pkgs.catppuccin-gtk.override {
+                  accents = [ "mauve" ];
+                  variant = "mocha";
+                };
+              };
+              iconTheme = lib.mkForce {
+                name = "Adwaita";
+                package = pkgs.adwaita-icon-theme;
+              };
             };
           };
       };
