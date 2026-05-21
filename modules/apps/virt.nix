c392a68aa4edb6a5edd0b2eb3e2c2ab4cf745cbc
greyxp1 <greyxp999@gmail.com>
2026-05-21 16:04:05 -0400

    major overhaul

--- modules/apps/virt.nix
+++ modules/apps/virt.nix
@@ -11,156 +11,78 @@
       config = lib.mkIf (config.networking.hostName == "desktop") {
 
         environment.systemPackages = with pkgs; [
-          virt-manager
-          spice
           spice-gtk
-          spice-protocol
-          virtio-win
+
+          (pkgs.writeScriptBin "create-nixos-vm" ''
+            #!/bin/sh
+            if [ -z "$1" ] || [ -z "$2" ]; then
+              echo "Usage: create-nixos-vm <vm-name> <path-to-nixos-iso>"
+              exit 1
+            fi
+
+            virt-install \
+              --connect qemu:///system \
+              --name "$1" \
+              --memory 8192 \
+              --vcpus 4 \
+              --disk size=40,pool=default,bus=virtio \
+              --os-variant=nixos-unstable \
+              --boot uefi \
+              --network network=default,model=virtio \
+              --noautoconsole \
+              --cdrom "$2" \
+              --video virtio,accel3d=on \
+              --graphics spice,listen=none,image.compression=off \
+              --graphics egl-headless,gl.rendernode=/dev/dri/renderD128
+          '')
         ];
 
-        virtualisation = {
-          libvirtd = {
-            enable = true;
-            qemu.swtpm.enable = true;
-          };
+        # Ensure the user has full hardware access to standard render nodes
+        users.users.grey.extraGroups = [
+          "libvirtd"
+          "video"
+          "render"
+        ];
 
-          vmVariant = {
-            spiceUSBRedirection.enable = true;
-            virtualisation.qemu.options = [
-              "-device virtio-vga-gl"
-              "-display gtk,gl=on"
-              "-cpu host"
-            ];
+        programs.virt-manager.enable = true;
+        programs.dconf.enable = true;
+
+        programs.dconf.profiles.user.databases = [
+          {
+            settings = {
+              "org/virt-manager/virt-manager/connections" = {
+                autoconnect = [ "qemu:///system" ];
+                uris = [ "qemu:///system" ];
+              };
+            };
+          }
+        ];
+
+        virtualisation.libvirtd = {
+          enable = true;
+          qemu = {
+            runAsRoot = true;
+
+            # Fixes EGL_NOT_INITIALIZED: Exposes the host's Nvidia/OpenGL driver libraries to QEMU's sandbox
+            package = pkgs.qemu_kvm.overrideAttrs (oldAttrs: {
+              nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ pkgs.makeWrapper ];
+              postInstall = (oldAttrs.postInstall or "") + ''
+                wrapProgram $out/bin/qemu-system-x86_64 \
+                  --prefix LD_LIBRARY_PATH : /run/opengl-driver/lib
+              '';
+            });
+
+            verbatimConfig = ''
+              cgroup_device_acl = [
+                  "/dev/null", "/dev/full", "/dev/zero",
+                  "/dev/random", "/dev/urandom",
+                  "/dev/ptmx", "/dev/kvm",
+                  "/dev/nvidiactl", "/dev/nvidia0",
+                  "/dev/nvidia-modeset", "/dev/dri/renderD128"
+              ]
+            '';
           };
         };
-
-        home-manager.users.grey =
-          { pkgs, ... }:
-          {
-            systemd.user.services.define-nixos-vm = {
-              Unit = {
-                Description = "Define nixos-vm in libvirt user session";
-                After = [ "default.target" ];
-              };
-              Service = {
-                Type = "oneshot";
-                RemainAfterExit = true;
-                ExecStart = pkgs.writeShellScript "define-nixos-vm" ''
-                  VM_DIR="$HOME/.local/share/libvirt/images"
-                  VM_XML="$HOME/.local/share/libvirt/nixos-vm.xml"
-                  DISK="$VM_DIR/nixos-vm.qcow2"
-
-                  mkdir -p "$VM_DIR"
-                  mkdir -p "$HOME/.local/share/libvirt/qemu/nvram"
-
-                  NVRAM="$HOME/.local/share/libvirt/qemu/nvram/nixos-vm_VARS.fd"
-                  if [ ! -f "$NVRAM" ]; then
-                    cp /run/libvirt/nix-ovmf/edk2-i386-vars.fd "$NVRAM"
-                    chmod 600 "$NVRAM"
-                  fi
-
-                  ISO="$VM_DIR/nixos-installer.iso"
-                  ISO_URL="https://channels.nixos.org/nixos-unstable/latest-nixos-minimal-x86_64-linux.iso"
-                  if [ ! -f "$ISO" ]; then
-                    echo "Downloading NixOS ISO..."
-                    ${pkgs.curl}/bin/curl -L --fail -o "$ISO.tmp" "$ISO_URL" && mv "$ISO.tmp" "$ISO"
-                  fi
-
-                  if [ ! -f "$DISK" ]; then
-                    ${pkgs.qemu}/bin/qemu-img create -f qcow2 "$DISK" 40G
-                  fi
-
-                  cat > "$VM_XML" << XMLEOF
-                  <domain type='kvm'>
-                    <name>nixos-vm</name>
-                    <memory unit='GiB'>4</memory>
-                    <currentMemory unit='GiB'>4</currentMemory>
-                    <vcpu placement='static'>4</vcpu>
-                    <os>
-                      <type arch='x86_64' machine='q35'>hvm</type>
-                      <loader readonly='yes' type='pflash'>/run/libvirt/nix-ovmf/edk2-x86_64-code.fd</loader>
-                      <nvram template='/run/libvirt/nix-ovmf/edk2-i386-vars.fd'>$HOME/.local/share/libvirt/qemu/nvram/nixos-vm_VARS.fd</nvram>
-                      <boot dev='cdrom'/>
-                      <boot dev='hd'/>
-                    </os>
-                    <features>
-                      <acpi/>
-                      <apic/>
-                    </features>
-                    <cpu mode='host-passthrough' check='none' migratable='on'/>
-                    <clock offset='utc'>
-                      <timer name='rtc' tickpolicy='catchup'/>
-                      <timer name='pit' tickpolicy='delay'/>
-                      <timer name='hpet' present='no'/>
-                    </clock>
-                    <on_poweroff>destroy</on_poweroff>
-                    <on_reboot>restart</on_reboot>
-                    <on_crash>destroy</on_crash>
-                    <devices>
-                      <emulator>/run/libvirt/nix-emulators/qemu-system-x86_64</emulator>
-                      <disk type='file' device='disk'>
-                        <driver name='qemu' type='qcow2' discard='unmap'/>
-                        <source file='$DISK'/>
-                        <target dev='vda' bus='virtio'/>
-                      </disk>
-                      <controller type='usb' index='0' model='qemu-xhci' ports='15'/>
-                      <controller type='pci' index='0' model='pcie-root'/>
-                      <controller type='pci' index='1' model='pcie-root-port'/>
-                      <controller type='pci' index='2' model='pcie-root-port'/>
-                      <controller type='pci' index='3' model='pcie-root-port'/>
-                      <interface type='user'>
-                        <mac address='52:54:00:ab:cd:ef'/>
-                        <model type='virtio'/>
-                      </interface>
-                      <channel type='spicevmc'>
-                        <target type='virtio' name='com.redhat.spice.0'/>
-                      </channel>
-                      <input type='tablet' bus='usb'/>
-                      <input type='keyboard' bus='usb'/>
-                      <graphics type='spice'>
-                        <listen type='none'/>
-                        <image compression='off'/>
-                      </graphics>
-                      <graphics type='egl-headless'>
-                        <gl rendernode='/dev/dri/renderD128'/>
-                      </graphics>
-                      <sound model='ich9'/>
-                      <audio id='1' type='spice'/>
-                      <video>
-                        <model type='virtio' heads='1' primary='yes'>
-                          <acceleration accel3d='yes'/>
-                        </model>
-                      </video>
-                      <disk type='file' device='cdrom'>
-                        <driver name='qemu' type='raw'/>
-                        <source file='$ISO'/>
-                        <target dev='sda' bus='sata'/>
-                        <readonly/>
-                      </disk>
-                      <serial type='pty'>
-                        <target type='isa-serial' port='0'/>
-                      </serial>
-                      <console type='pty'>
-                        <target type='serial' port='0'/>
-                      </console>
-                      <redirdev bus='usb' type='spicevmc'/>
-                      <redirdev bus='usb' type='spicevmc'/>
-                      <memballoon model='virtio'/>
-                      <rng model='virtio'>
-                        <backend model='random'>/dev/urandom</backend>
-                      </rng>
-                    </devices>
-                  </domain>
-                  XMLEOF
-
-                  ${pkgs.libvirt}/bin/virsh -c qemu:///session destroy nixos-vm 2>/dev/null || true
-                  ${pkgs.libvirt}/bin/virsh -c qemu:///session undefine --nvram nixos-vm 2>/dev/null || true
-                  ${pkgs.libvirt}/bin/virsh -c qemu:///session define "$VM_XML" || true
-                '';
-              };
-              Install.WantedBy = [ "default.target" ];
-            };
-          };
       };
     };
 }
