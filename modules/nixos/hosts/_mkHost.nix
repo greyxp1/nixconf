fc5e2da334e26e5c3c5b9b13e5cedc39680e8b39
greyxp1 <greyxp999@gmail.com>
2026-05-25 05:26:00 -0400

    added helper to reduce boilderplate duplication

---
+++ modules/nixos/hosts/_mkHost.nix
@@ -0,0 +1,8 @@
+inputs: { hostModule, extraModules ? [ ] }:
+inputs.nixpkgs.lib.nixosSystem {
+  system = "x86_64-linux";
+  specialArgs = { inherit inputs; };
+  modules = [ hostModule ]
+    ++ builtins.attrValues inputs.self.nixosModules
+    ++ extraModules;
+}
