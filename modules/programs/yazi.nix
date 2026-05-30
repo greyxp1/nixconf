5aa6353c92d5db95c1c20376a9f2f0258105cbd0
greyxp1 <greyxp999@gmail.com>
2026-05-30 00:11:57 -0400

    Update yazi.nix

--- modules/programs/yazi.nix
+++ modules/programs/yazi.nix
@@ -8,27 +8,29 @@
         extraPackages = with pkgs; [ ripdrag trash-cli ];
 
         plugins = with pkgs.yaziPlugins; {
-          inherit smart-enter jump-to-char full-border mount toggle-pane compress restore;
+          inherit smart-enter jump-to-char full-border mount toggle-pane compress restore starship;
         };
 
-        initLua = ''require("full-border"):setup({ type = ui.Border.ROUNDED })'';
+        initLua = ''
+          require("full-border"):setup({ type = ui.Border.ROUNDED })
+          require("smart-enter"):setup({ open_multi = true })
+          require("starship"):setup()
+        '';
 
-        settings = {
-          mgr = {
-            ratio   = [ 1 2 5 ];
-            sort_by = "natural";
-          };
+        settings.mgr = {
+          ratio   = [ 1 2 5 ];
+          sort_by = "natural";
         };
 
         keymap.mgr.prepend_keymap = [
-          { on = [ "<l>" ];   run = "plugin smart-enter";             desc = "Enter dir or open file"; }
-          { on = [ "f" ];     run = "plugin jump-to-char";            desc = "Jump to filename starting with char"; }
-          { on = [ "C" ];     run = "plugin compress";                desc = "Compress selected files"; }
-          { on = [ "u" ];     run = "plugin restore";                 desc = "Restore last deleted file"; }
-          { on = [ "<A-m>" ]; run = "plugin mount";                   desc = "Mount manager"; }
-          { on = [ "<A-p>" ]; run = "plugin toggle-pane min-preview"; desc = "Hide/show preview pane"; }
-          { on = [ "M" ];     run = "plugin toggle-pane max-preview"; desc = "Maximize/restore preview pane"; }
-          { on = [ "<A-d>" ]; run = ''shell -- ripdrag --and-exit --target --all "$@" | while read -r fp; do cp -nR "$fp" .; done''; desc = "Drag-and-drop (bidirectional)"; }
+          { on = [ "l" ];       run = "plugin smart-enter";             desc = "Enter dir or open file"; }
+          { on = [ "f" ];       run = "plugin jump-to-char";            desc = "Jump to filename starting with char"; }
+          { on = [ "C" ];       run = "plugin compress";                desc = "Compress selected files"; }
+          { on = [ "u" ];       run = "plugin restore";                 desc = "Restore last deleted file"; }
+          { on = [ "<A-m>" ];   run = "plugin mount";                   desc = "Mount manager"; }
+          { on = [ "<A-p>" ];   run = "plugin toggle-pane min-preview"; desc = "Hide/show preview pane"; }
+          { on = [ "M" ];       run = "plugin toggle-pane max-preview"; desc = "Maximize/restore preview pane"; }
+          { on = [ "<A-d>" ];   run = ''shell -- ripdrag --and-exit --target --all "$@" | while read -r fp; do cp -nR "$fp" .; done''; desc = "Drag-and-drop (bidirectional)"; }
         ];
       };
     };
