cd4eb261fc21de8555ded31f152b35d7f66a52c1
greyxp1 <greyxp999@gmail.com>
2026-06-02 14:15:49 -0400

    moved performance settings to core.nix and deleted redundant settings

--- modules/system/core.nix
+++ modules/system/core.nix
@@ -5,24 +5,25 @@
     networking.networkmanager.enable = true;
     nixpkgs.config.allowUnfree = true;
     documentation.nixos.enable = false;
-    security.polkit.enable = true;
-    security.sudo.wheelNeedsPassword = false;
     system.nixos.label = config.networking.hostName;
     system.stateVersion = "25.11";
     zramSwap.enable = true;
     zramSwap.algorithm = "zstd";
-    nix.settings.trusted-users = [ "root" "@wheel" ];
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+    nix.settings = {
+      trusted-users = [ "root" "@wheel" ];
+      experimental-features = [ "nix-command" "flakes" ];
+      max-jobs = "auto";
+      cores = 0;
+      http-connections = 128;
+      download-buffer-size = 536870912;
+    };
 
     services = {
-      upower.enable = true;
-      power-profiles-daemon.enable = true;
+      irqbalance.enable = true;
       dbus.enable = true;
-      gvfs.enable = true;
       greetd = {
         enable = true;
-        useTextGreeter = true;
-        restart = false;
         settings.default_session.command = "niri-session";
         settings.default_session.user = "grey";
       };
@@ -39,6 +40,23 @@
       enableRedistributableFirmware = true;
       graphics.enable = true;
       graphics.enable32Bit = true;
+    };
+
+    security = {
+      polkit.enable = true;
+      sudo.wheelNeedsPassword = false;
+      rtkit.enable = true;
+    };
+
+    boot.kernel.sysctl = {
+      "vm.swappiness" = 100;
+      "vm.page-cluster" = 0;
+      "vm.vfs_cache_pressure" = 50;
+      "vm.dirty_ratio" = 10;
+      "vm.dirty_background_ratio" = 5;
+      "net.core.rmem_max" = 16777216;
+      "net.core.wmem_max" = 16777216;
+      "net.core.netdev_max_backlog" = 16384;
     };
   };
 }
