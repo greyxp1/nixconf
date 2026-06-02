004dfdb37f5c99c598bee9bfe82b5d7ca8a559fd
greyxp1 <greyxp999@gmail.com>
2026-06-02 14:26:29 -0400

    Update noctalia.nix

--- modules/programs/noctalia/noctalia.nix
+++ modules/programs/noctalia/noctalia.nix
@@ -2,7 +2,7 @@
   flake.nixosModules.noctalia = { pkgs, ... }: {
     home-manager.users.grey.xdg.configFile."noctalia/config.toml".source = ./config.toml;
     environment.systemPackages = with pkgs; [
-      inputs.noctalia.packages.${system}.default
+      inputs.noctalia.packages.${stdenv.hostPlatform.system}.default
       gpu-screen-recorder
     ];
   };
