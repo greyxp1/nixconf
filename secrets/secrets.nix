a05be8d88d622bb7f6080779875049fbe99f6d1e
greyxp1 <greyxp999@gmail.com>
2026-06-02 11:25:20 -0400

    added secrets

---
+++ secrets/secrets.nix
@@ -0,0 +1,8 @@
+{
+  "ssh.age".publicKeys = [
+    "age1s9r5qqy0ce97xh2mrqrt0wlwg72ltnyqtzs3wtxpzcyqg8272yts5nyv8r" # master
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIDaMK68lfk2MrpJz8wFC98dCX91nAiCWMQ6K4SVeIhY" # desktop
+    "ssh-ed25519 AAAA..." # vm
+    "ssh-ed25519 AAAA..." # generic
+  ];
+}
