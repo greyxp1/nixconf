23857ba17dc98a0ab570c16497683aac6d29635d
greyxp1 <greyxp999@gmail.com>
2026-06-03 14:41:34 -0400

    added helix.nix

---
+++ modules/programs/helix.nix
@@ -0,0 +1,20 @@
+{...}: {
+  flake.nixosModules.helix = {...}: {
+    home-manager.users.grey = {lib, ...}: {
+      home.sessionVariables.EDITOR = "hx";
+      home.sessionVariables.VISUAL = "hx";
+      programs.helix = {
+        enable = true;
+        settings = {
+          theme = lib.mkForce "catppuccin_transparent";
+        };
+        themes = {
+          catppuccin_transparent = {
+            inherits = "catppuccin_mocha";
+            "ui.background" = {bg = "none";};
+          };
+        };
+      };
+    };
+  };
+}
