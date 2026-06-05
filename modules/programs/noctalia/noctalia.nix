99cd989fcdcbb016a60e2c2ed2127a60cab9a821
greyxp1 <greyxp999@gmail.com>
2026-06-05 18:58:34 -0400

    home manager update

--- modules/programs/noctalia/noctalia.nix
+++ modules/programs/noctalia/noctalia.nix
@@ -1,9 +1,20 @@
 {inputs, ...}: {
-  flake.nixosModules.noctalia = {pkgs, ...}: {
-    home-manager.users.grey.xdg.configFile."noctalia/config.toml".source = ./config.toml;
-    environment.systemPackages = with pkgs; [
-      inputs.noctalia.packages.${stdenv.hostPlatform.system}.default
-      gpu-screen-recorder
-    ];
+  flake.nixosModules.noctalia = {pkgs, ...}: let
+    noctaliaPkg = inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default;
+  in {
+    environment.systemPackages = [pkgs.gpu-screen-recorder];
+    home-manager.users.grey = {
+      imports = [inputs.noctalia.homeModules.default];
+      programs.noctalia = {
+        enable = true;
+        package = noctaliaPkg;
+        settings = ./config.toml;
+        systemd.enable = true;
+      };
+      systemd.user.services.noctalia.Service.ExecStartPost = "${pkgs.writeShellScript "noctalia-init-replay" ''
+        sleep 0.5
+        ${noctaliaPkg}/bin/noctalia msg scripted-widget screen_recorder focused replay-start
+      ''}";
+    };
   };
 }
