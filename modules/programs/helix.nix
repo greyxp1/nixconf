dff9d0a03f680bbc41c0acc7216e2c10b31d6216
greyxp1 <greyxp999@gmail.com>
2026-06-05 18:59:31 -0400

    added helix

--- modules/programs/helix.nix
+++ modules/programs/helix.nix
@@ -5,12 +5,18 @@
       home.sessionVariables.VISUAL = "hx";
       programs.helix = {
         enable = true;
-        settings = {theme = lib.mkForce "catppuccin_transparent";};
-        themes = {
-          catppuccin_transparent = {
-            inherits = "catppuccin_mocha";
-            "ui.background" = {bg = "none";};
+        settings = {
+          theme = lib.mkForce "catppuccin_transparent";
+          editor.cursor-shape = {
+            normal = "underline";
+            insert = "bar";
+            select = "underline";
           };
+        };
+
+        themes.catppuccin_transparent = {
+          inherits = "catppuccin_mocha";
+          "ui.background" = {bg = "none";};
         };
       };
     };
