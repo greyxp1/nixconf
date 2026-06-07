8b1264c4720318825fea283bf91494d91683cccf
greyxp1 <greyxp999@gmail.com>
2026-06-07 03:55:10 -0400

    removed session variables

--- modules/programs/helix.nix
+++ modules/programs/helix.nix
@@ -1,8 +1,6 @@
 {...}: {
   flake.nixosModules.helix = {...}: {
     home-manager.users.grey = {lib, ...}: {
-      home.sessionVariables.EDITOR = "hx";
-      home.sessionVariables.VISUAL = "hx";
       programs.helix = {
         enable = true;
         settings = {
