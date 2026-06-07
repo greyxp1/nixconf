12a528db2e8daed0eb65315ea3868eca3ae257b7
greyxp1 <greyxp999@gmail.com>
2026-06-07 13:28:18 -0400

    fixed mounting

--- modules/programs/shell/yazi.nix
+++ modules/programs/shell/yazi.nix
@@ -19,6 +19,15 @@
       }
     ];
 
+    services.udisks2.enable = true;
+    security.polkit.extraConfig = ''
+      polkit.addRule(function(action, subject) {
+        if (subject.isInGroup("wheel") && action.id.startsWith("org.freedesktop.udisks2.")) {
+          return polkit.Result.YES;
+        }
+      });
+    '';
+
     home-manager.users.grey = {...}: {
       programs.yazi = {
         enable = true;
