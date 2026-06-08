1af173fb1114c659ae76c4a6df651e22d37fed8d
greyxp1 <greyxp999@gmail.com>
2026-06-08 17:08:12 -0400

    moved comma to cli.nix

--- modules/programs/shell/cli.nix
+++ modules/programs/shell/cli.nix
@@ -1,4 +1,4 @@
-{...}: {
+{inputs, ...}: {
   flake.nixosModules.shell = {pkgs, ...}: let
     starshipTheme = fromTOML (builtins.readFile ./starship.toml);
   in {
@@ -11,47 +11,53 @@
       };
     };
 
-    home-manager.users.grey = {...}: {
-      home.sessionVariables = {
-        MANPAGER = "sh -c 'col -bx | bat -l man -p'";
-        PAGER = "bat -p";
+    home-manager = {
+      sharedModules = [inputs.nix-index-database.homeModules.nix-index];
+      users.grey = {...}: {
+        home.sessionVariables = {
+          MANPAGER = "sh -c 'col -bx | bat -l man -p'";
+          PAGER = "bat -p";
+        };
+
+        programs = {
+          nix-index.enable = true;
+          nix-index-database.comma.enable = true;
+
+          zoxide = {
+            enable = true;
+            enableNushellIntegration = true;
+            options = ["--cmd cd"];
+          };
+
+          starship = {
+            enable = true;
+            enableNushellIntegration = true;
+            settings = starshipTheme;
+          };
+
+          bottom.enable = true;
+          bottom.settings.flags = {
+            group_processes = true;
+            process_memory_as_value = true;
+            case_sensitive = false;
+            regex = true;
+          };
+        };
+
+        home.packages = with pkgs; [
+          curl
+          lstr
+          bat
+          fastfetch
+          zip
+          unzip
+          wget
+          codex
+          nerd-fonts.jetbrains-mono
+          fzf
+          dash
+        ];
       };
-
-      programs = {
-        zoxide = {
-          enable = true;
-          enableNushellIntegration = true;
-          options = ["--cmd cd"];
-        };
-
-        starship = {
-          enable = true;
-          enableNushellIntegration = true;
-          settings = starshipTheme;
-        };
-
-        bottom.enable = true;
-        bottom.settings.flags = {
-          group_processes = true;
-          process_memory_as_value = true;
-          case_sensitive = false;
-          regex = true;
-        };
-      };
-
-      home.packages = with pkgs; [
-        curl
-        lstr
-        bat
-        fastfetch
-        zip
-        unzip
-        wget
-        codex
-        nerd-fonts.jetbrains-mono
-        fzf
-        dash
-      ];
     };
   };
 }
