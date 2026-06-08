7939340c22d4142412b228ac390869d740f07bb2
greyxp1 <greyxp999@gmail.com>
2026-06-07 20:17:51 -0400

    chore: enroll vm

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,7 @@
   "ssh.age".publicKeys = [
     "age1s9r5qqy0ce97xh2mrqrt0wlwg72ltnyqtzs3wtxpzcyqg8272yts5nyv8r" # master
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIDaMK68lfk2MrpJz8wFC98dCX91nAiCWMQ6K4SVeIhY" # desktop
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPGVZL7u7Ty/DlqcKaytV1m9V4cVkJzPUcQ1KtHUfu6b" # vm
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKd0UykyYwrBhbYzhldK6XobJIpiJJIAgt5lukApErks" # vm
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPi6G2BqcPe6nlGg4dbnkPJ2y3QTP2XIMe0ulnKa72+D" # generic
   ];
 }
