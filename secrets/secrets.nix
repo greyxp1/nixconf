99c87de60648c469066e4e4439805a110446efde
greyxp1 <greyxp999@gmail.com>
2026-06-11 18:19:10 -0400

    chore: enroll desktop

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 {
   "ssh.age".publicKeys = [
     "age1s9r5qqy0ce97xh2mrqrt0wlwg72ltnyqtzs3wtxpzcyqg8272yts5nyv8r" # master
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIDaMK68lfk2MrpJz8wFC98dCX91nAiCWMQ6K4SVeIhY" # desktop
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOrbBuRQjEzkHhyonE0NnxilUW2dOZTAqTB95YVMXJG9" # desktop
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII0dPxbgu7poPhZOGFL5EKD5Xymq+IIflIsYltV5+uMH" # vm
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPi6G2BqcPe6nlGg4dbnkPJ2y3QTP2XIMe0ulnKa72+D" # generic
   ];
