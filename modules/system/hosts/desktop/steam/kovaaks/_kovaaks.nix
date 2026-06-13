93ec11920e0bc0199c4a77ce3193047cc12eeaf2
greyxp1 <greyxp999@gmail.com>
2026-06-12 23:31:38 -0400

    Update _kovaaks.nix

--- modules/system/hosts/desktop/steam/kovaaks/_kovaaks.nix
+++ modules/system/hosts/desktop/steam/kovaaks/_kovaaks.nix
@@ -16,13 +16,19 @@ _: {
   in {
     home.activation.kovaaks = lib.hm.dag.entryAfter ["writeBoundary"] ''
       if [ -d "${dst}" ]; then
-        $VERBOSE_ECHO "Deploying Kovaak's config..."
-        $DRY_RUN_CMD rm -rf "${dst}/crosshairs"
-        $DRY_RUN_CMD cp -rT "${src}/crosshairs" "${dst}/crosshairs"
-        $DRY_RUN_CMD rm -rf "${dst}/Saved/SaveGames/Themes"
-        $DRY_RUN_CMD cp -rT "${src}/Themes" "${dst}/Saved/SaveGames/Themes"
-        $DRY_RUN_CMD install -Dm644 "${src}/PrimaryUserSettings.json" "${dst}/PrimaryUserSettings.json"
-        $DRY_RUN_CMD install -Dm644 "${src}/weaponsettings.ini" "${dst}/weaponsettings.ini"
+        _stamp="${dst}/.nixconf-stamp"
+        if [ "$(cat "$_stamp" 2>/dev/null)" != "${src}" ]; then
+          $VERBOSE_ECHO "Deploying Kovaak's config (src: ${src})..."
+          $DRY_RUN_CMD rm -rf "${dst}/crosshairs"
+          $DRY_RUN_CMD cp -rT "${src}/crosshairs" "${dst}/crosshairs"
+          $DRY_RUN_CMD rm -rf "${dst}/Saved/SaveGames/Themes"
+          $DRY_RUN_CMD cp -rT "${src}/Themes" "${dst}/Saved/SaveGames/Themes"
+          $DRY_RUN_CMD install -Dm644 "${src}/PrimaryUserSettings.json" "${dst}/PrimaryUserSettings.json"
+          $DRY_RUN_CMD install -Dm644 "${src}/weaponsettings.ini" "${dst}/weaponsettings.ini"
+          [ -z "$DRY_RUN_CMD" ] && printf '%s' "${src}" > "$_stamp"
+        else
+          $VERBOSE_ECHO "Kovaak's config up to date, skipping."
+        fi
       fi
     '';
   };
