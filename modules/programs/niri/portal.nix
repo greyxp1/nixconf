17fd3220d75275e1669a410a42c645d73f860248
greyxp1 <greyxp999@gmail.com>
2026-06-15 01:04:19 -0400

    simplified and commented out the niri autoselect portal and added the gnome portal

--- modules/programs/niri/portal.nix
+++ modules/programs/niri/portal.nix
@@ -1,28 +1,7 @@
 {inputs, ...}: {
-  flake.nixosModules.niri-autoselect-portal = {pkgs, ...}: let
-    screencastMonitor = pkgs.writeScript "screencast-monitor" ''
-      #!${pkgs.dash}/bin/dash
-      dbus-monitor --session \
-        "type='method_call',interface='org.freedesktop.portal.ScreenCast',member='Start'" \
-      | grep --line-buffered "method call" \
-      | while IFS= read -r _; do
-          niri msg action set-dynamic-cast-monitor
-        done
-    '';
-  in {
-    imports = [inputs.niri-autoselect-portal.nixosModules.default];
-    services.niri-autoselect-portal.enable = true;
-    home-manager.sharedModules = [
-      {
-        systemd.user.services.screencast-monitor = {
-          Unit.Description = "Auto-set niri dynamic cast target on screenshare";
-          Unit.After = ["graphical-session.target"];
-          Unit.PartOf = ["graphical-session.target"];
-          Service.ExecStart = "${screencastMonitor}";
-          Service.Restart = "on-failure";
-          Install.WantedBy = ["graphical-session.target"];
-        };
-      }
-    ];
+  flake.nixosModules.niri-portal = {...}: {
+    #imports = [inputs.niri-autoselect-portal.nixosModules.default];
+    #services.niri-autoselect-portal.enable = true;
+    xdg.portal.config.niri.default = ["gnome"];
   };
 }
