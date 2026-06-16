01866353671394e0ad7a528e7a6d5fd7c62d792b
greyxp1 <greyxp999@gmail.com>
2026-06-16 08:55:35 -0400

    removed useless setting

--- modules/system/hosts/desktop/_nvidia.nix
+++ modules/system/hosts/desktop/_nvidia.nix
@@ -6,7 +6,6 @@
   environment.systemPackages = [pkgs.nvidia-vaapi-driver];
   boot.initrd.kernelModules = ["nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm"];
   boot.kernelParams = ["nvidia_drm.modeset=1"];
-  boot.extraModprobeConfig = "options nvidia NVreg_DynamicPowerManagementVideoMemoryThreshold=0"; # Niri
   services.xserver.videoDrivers = ["nvidia"];
   services.acpid.enable = lib.mkForce false;
   hardware.nvidia = {
