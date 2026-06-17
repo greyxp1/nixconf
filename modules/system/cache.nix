37a3e4e62d6d4123ade521b44f6551c661ec7a5f
greyxp1 <greyxp999@gmail.com>
2026-06-17 19:49:55 -0400

    added nyx cache

--- modules/system/cache.nix
+++ modules/system/cache.nix
@@ -10,6 +10,7 @@
         "https://cache.garnix.io"
         "https://catppuccin.cachix.org"
         "https://yazi.cachix.org"
+        "https://nyx.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
@@ -20,6 +21,7 @@
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
         "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
+        "nyx.cachix.org-1:xH6G0MO9PrpeGe7mHBtj1WbNzmnXr7jId2mCiq6hipE="
       ];
     };
   };
