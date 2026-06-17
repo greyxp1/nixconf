a41ea58dfb635d2bb1849b4ed5b77e9ccf2c14e4
greyxp1 <greyxp999@gmail.com>
2026-06-17 19:25:24 -0400

    removed inputs

--- modules/programs/niri/portal.nix
+++ modules/programs/niri/portal.nix
@@ -1,4 +1,4 @@
-{inputs, ...}: {
+{...}: {
   flake.nixosModules.niri-portal = {...}: {
     #imports = [inputs.niri-autoselect-portal.nixosModules.default];
     #services.niri-autoselect-portal.enable = true;
