849cb65c59a82c8b0f968186e6f7f3a741dc7491
greyxp1 <greyxp999@gmail.com>
2026-06-18 00:02:43 -0400

    refactor

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -2,31 +2,33 @@
   flake.nixosModules.git = {...}: {
     home-manager.users.grey = {...}: {
       programs = {
-        git.enable = true;
-        git.settings = {
-          init.defaultBranch = "main";
-          column.ui = "auto";
-          pull.rebase = true;
-          branch.autosetuprebase = "always";
-          push.autoSetupRemote = true;
-          diff.algorithm = "histogram";
-          merge.conflictstyle = "zdiff3";
-
-          fetch = {
-            prune = true;
-            all = true;
-          };
-
-          user = {
-            name = "greyxp1";
-            email = "greyxp999@gmail.com";
+        git = {
+          enable = true;
+          settings = {
+            init.defaultBranch = "main";
+            column.ui = "auto";
+            pull.rebase = true;
+            branch.autosetuprebase = "always";
+            push.autoSetupRemote = true;
+            diff.algorithm = "histogram";
+            merge.conflictstyle = "zdiff3";
+            fetch = {
+              prune = true;
+              all = true;
+            };
+            user = {
+              name = "greyxp1";
+              email = "greyxp999@gmail.com";
+            };
           };
         };
 
         delta = {
           enable = true;
-          options.side-by-side = true;
-          options.line-numbers = true;
+          options = {
+            side-by-side = true;
+            line-numbers = true;
+          };
         };
       };
     };
