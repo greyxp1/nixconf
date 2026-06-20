ffd8db0ebbc7f8e4fbc76ecd6ad2f3609e2e1596
greyxp1 <greyxp999@gmail.com>
2026-06-20 19:19:45 -0400

    fixed dumb bug

--- modules/programs/starship/starship.nix
+++ modules/programs/starship/starship.nix
@@ -1,8 +1,8 @@
 {...}: {
   flake.nixosModules.shell = {...}: {
     home-manager = {
-      catppuccin.starship.enable = false;
       users.grey = {...}: {
+        catppuccin.starship.enable = false;
         programs.starship = {
           enable = true;
           settings = fromTOML (builtins.readFile ./starship.toml);
