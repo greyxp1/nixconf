4e2ecbe8b1334a024c7e619fef382af0e194b138
greyxp1 <greyxp999@gmail.com>
2026-06-20 17:41:57 -0400

    disabled catppuccin theme in favor of catppuccin blur, added icon theme and added freellmapi

--- modules/programs/zed.nix
+++ modules/programs/zed.nix
@@ -3,10 +3,13 @@
     programs.nix-ld.enable = true;
     home-manager.users.grey = {pkgs, ...}: {
       home.packages = with pkgs; [nixd nil alejandra];
+      catppuccin.zed.enable = false;
       programs.zed-editor = {
         enable = true;
         userSettings = {
           #helix_mode = true;
+          theme = "Catppuccin Mocha (Blur)";
+          icon_theme = "Catppuccin Mocha";
           buffer_font_family = "JetBrainsMono Nerd Font";
           session.trust_all_worktrees = true;
           collaboration_panel.button = false;
@@ -43,9 +46,31 @@
             };
             nil.initialization_options.formatting.command = ["alejandra" "--quiet" "--"];
           };
+
+          language_models.openai_compatible.FreeLLMAPI = {
+            api_url = "http://192.168.1.66:3001/v1";
+            available_models = [
+              {
+                name = "auto";
+                max_tokens = 200000;
+                max_output_tokens = 32000;
+                max_completion_tokens = 200000;
+                capabilities = {
+                  tools = true;
+                  images = false;
+                  parallel_tool_calls = false;
+                  prompt_cache_key = false;
+                  chat_completions = true;
+                  interleaved_reasoning = false;
+                };
+              }
+            ];
+          };
         };
 
         extensions = [
+          "catppuccin-icons"
+          "catppuccin-blur"
           "html"
           "git-firefly"
           "nix"
