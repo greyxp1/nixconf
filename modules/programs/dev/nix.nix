576290c0af923f58fc76dd5003e5f1325a69c48e
greyxp1 <greyxp999@gmail.com>
2026-06-23 20:10:55 -0400

    reduced redundancy and made format on save only work in certain projects

--- modules/programs/dev/nix.nix
+++ modules/programs/dev/nix.nix
@@ -1,37 +1,36 @@
 _: {
   flake.nixosModules.nix-language = _: {
     home-manager.users.grey = {pkgs, ...}: let
-      alejandra-patched = pkgs.alejandra.overrideAttrs (prev: {
+      formatProjects = ["/home/grey/nixconf"];
+      formatProjectPatterns = builtins.concatStringsSep "|" (map (project: "${project}/*") formatProjects);
+      nixosOptionsExpr = "(builtins.getFlake \"path:/home/grey/nixconf\")" + ".nixosConfigurations.desktop.options";
+      alejandra = pkgs.alejandra.overrideAttrs (prev: {
         patches = (prev.patches or []) ++ [./alejandra.patch];
         doCheck = false;
       });
 
       nix-format = pkgs.writeShellApplication {
         name = "nix-format";
-        runtimeInputs = with pkgs; [
-          alejandra-patched
-          deadnix
-          statix
-        ];
+        runtimeInputs = with pkgs; [alejandra deadnix statix];
         text = ''
           file=$(mktemp --suffix=.nix)
           trap 'rm -f "$file"' EXIT
           cat > "$file"
-          deadnix --edit -q "$file" >/dev/null 2>&1
-          statix fix "$file" >/dev/null 2>&1
-          alejandra -q "$file" >/dev/null
+          case "$PWD/" in
+            ${formatProjectPatterns})
+              deadnix --edit -q "$file" >/dev/null 2>&1
+              statix fix "$file" >/dev/null 2>&1
+              alejandra -q "$file" >/dev/null
+              ;;
+          esac
           cat "$file"
         '';
       };
     in {
-      home.packages = with pkgs; [
-        nix-format
-        nixd
-      ];
-
+      home.packages = with pkgs; [nix-format nixd];
       programs = {
-        helix.languages.language-server.nixd.config.options.nixos.expr = "(builtins.getFlake \"path:/home/grey/nixconf\")" + ".nixosConfigurations.desktop.options";
-        zed-editor.userSettings.lsp.nixd.initialization_options.nixos.expr = "(builtins.getFlake \"path:/home/grey/nixconf\")" + ".nixosConfigurations.desktop.options";
+        helix.languages.language-server.nixd.config.options.nixos.expr = nixosOptionsExpr;
+        zed-editor.userSettings.lsp.nixd.initialization_options.nixos.expr = nixosOptionsExpr;
       };
     };
   };
