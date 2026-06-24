1277c6ebbf677ea1f1332ef71ef7b1fa0ee96fd0
greyxp1 <greyxp999@gmail.com>
2026-06-24 18:33:43 -0400

    Update helium.nix

--- modules/programs/helium.nix
+++ modules/programs/helium.nix
@@ -1,57 +1,49 @@
 {inputs, ...}: {
   flake.nixosModules.helium = {...}: {
     imports = [inputs.helium.nixosModules.helium];
-    home-manager.users.grey = {...}: {
-      imports = [inputs.helium.homeModules.helium];
+    home-manager.users.grey = _: {
       programs.helium = {
         enable = true;
         defaultBrowser = true;
 
-        extraFlags = [
+        flags = [
           "--ozone-platform-hint=auto"
           "--enable-features=HeliumMiddleClickAutoscroll"
         ];
 
-        extraPolicies = {
-          ExtensionInstallForcelist = [
-            "ghmbeldphafepmbegfdlkpapadhbakde" # Proton Pass
-            "ldgfbffkinooeloadekpmfoklnobpien" # Raindrop.io
-            "mnjggcdmjocbbbhaepdhchncahnbgone" # SponsorBlock
-            "enamippconapkdmgfgjchkhakpfinmaj" # DeArrow
-            "lodcanccmfbpjjpnngindkkmiehimile" # Control Panel for YouTube
-            "eaeedemddlledlghhjebjgdmhjekgegd" # PiP View
-            "knemcdpkggnbhpoaaagmjiigenifejfo" # Claude Usage Tracker
-            "dkdnancajokhfclpjpplkhlkbhaeejob" # Claude QoL
-            "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
-            "effdbpeggelllpfkjppbokhmmiinhlmg" # Better Lyrics
-            "mcambpfmpjnncfoodejdmehedbkjepmi" # Equicord
-          ];
+        extensions = {
+          sponsorBlock.id = "mnjggcdmjocbbbhaepdhchncahnbgone";
+          deArrow.id = "enamippconapkdmgfgjchkhakpfinmaj";
+          controlPanel.id = "lodcanccmfbpjjpnngindkkmiehimile";
+          claudeUsageTracker.id = "knemcdpkggnbhpoaaagmjiigenifejfo";
+          claudeQoL.id = "dkdnancajokhfclpjpplkhlkbhaeejob";
+          betterLyrics.id = "effdbpeggelllpfkjppbokhmmiinhlmg";
+          equicord.id = "mcambpfmpjnncfoodejdmehedbkjepmi";
 
-          ExtensionSettings = {
-            "ghmbeldphafepmbegfdlkpapadhbakde".toolbar_pin = "force_pinned"; # Proton Pass
-            "ldgfbffkinooeloadekpmfoklnobpien".toolbar_pin = "force_pinned"; # Raindrop.io
-            "eaeedemddlledlghhjebjgdmhjekgegd".toolbar_pin = "force_pinned"; # PiP View
-            "eimadpbcbfnmbkopoojfekhnkhdbieeh".toolbar_pin = "force_pinned"; # Dark Reader
-          };
+          protonPass.id = "ghmbeldphafepmbegfdlkpapadhbakde";
+          protonPass.pin = true;
+
+          raindrop.id = "ldgfbffkinooeloadekpmfoklnobpien";
+          raindrop.pin = true;
+
+          pipView.id = "eaeedemddlledlghhjebjgdmhjekgegd";
+          pipView.pin = true;
+
+          darkReader.id = "eimadpbcbfnmbkopoojfekhnkhdbieeh";
+          darkReader.pin = true;
         };
-
-        # For later if I ever add something with a native-messaging companion app:
-        # nativeMessagingHosts = [pkgs.keepassxc];
 
         preferences = {
           browser.show_forward_button = false;
-          helium = {
-            #completed_onboarding = true;
-            browser = {
-              layout = 2;
-              show_avatar_button = false;
-              show_back_button = false;
-              show_reload_button = false;
-              show_vertical_tabs_collapse_button = false;
-              zen_mode = true;
-              zen_mode_sidebar_pinned = true;
-              zen_mode_top_chrome_pinned = true;
-            };
+          helium.browser = {
+            layout = 2;
+            show_avatar_button = false;
+            show_back_button = false;
+            show_reload_button = false;
+            show_vertical_tabs_collapse_button = false;
+            zen_mode = true;
+            zen_mode_sidebar_pinned = true;
+            zen_mode_top_chrome_pinned = true;
           };
           #bookmark_bar = {
           #  show_apps_shortcut = false;
