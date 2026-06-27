8df36d82c6dd9e5dddbd1a0473dac1a077c64b4e
greyxp1 <greyxp999@gmail.com>
2026-06-27 18:14:56 -0400

    removed yazi and nyx cache

--- modules/system/_cache.nix
+++ modules/system/_cache.nix
@@ -7,8 +7,6 @@
     "https://attic.xuyh0120.win/lantian"
     "https://cache.garnix.io"
     "https://catppuccin.cachix.org"
-    "https://yazi.cachix.org"
-    "https://nyx.cachix.org"
   ];
   trusted-public-keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
@@ -18,7 +16,5 @@
     "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
     "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
-    "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
-    "nyx.cachix.org-1:xH6G0MO9PrpeGe7mHBtj1WbNzmnXr7jId2mCiq6hipE="
   ];
 }
