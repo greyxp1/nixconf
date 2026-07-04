d2a07098b7b190f1e5e975e71e72dbdb00e999da
greyxp1 <greyxp999@gmail.com>
2026-07-04 03:36:08 -0400

    replicated full prism setup I had on windows

--- modules/programs/prism.nix
+++ modules/programs/prism.nix
@@ -1,16 +1,43 @@
 {
-  flake.homeModules.prism = {
+  flake.homeModules.prism = {pkgs, ...}: let
+    themeName = "Fluent-Dark";
+
+    fluentDarkTheme = pkgs.fetchFromGitHub {
+      owner = "M3CHR0M4NC3R";
+      repo = "Prism-Launcher-Themes";
+      rev = "main";
+      sha256 = "1r0nbcyj3yna4jh5qvflxmg25xcymilp00iw0adq6n54774bsz84";
+    };
+
+    fluentDarkIcons = pkgs.fetchFromGitHub {
+      owner = "PrismLauncher";
+      repo = "Themes";
+      rev = "main";
+      sha256 = "0gzkdzvkxz0s915dsj975l0b1xxk57vjm5jc1g9g80wmshia9aap";
+    };
+  in {
+    xdg.dataFile."PrismLauncher/iconthemes/${themeName}" = {
+      source = "${fluentDarkIcons}/icons/${themeName}";
+      recursive = true;
+    };
+
     programs.prismlauncher = {
       enable = true;
-      settings = {
-        MaxMemAlloc = 8192;
-        MinMemAlloc = 4096
-        StatusBarVisible = false;
-        
-      };
+      themes.${themeName} = "${fluentDarkTheme}/themes/${themeName}";
 
-      icons = ["Fluent-Dark"];
-      themes = ["Fluent-Dark"];
+      settings = {
+        Language = "en_US";
+        ApplicationTheme = themeName;
+        IconTheme = themeName;
+        AutomaticJavaDownload = true;
+        AutomaticJavaSwitch = true;
+        IgnoreJavaWizard = true;
+        MaxMemAlloc = 8192;
+        MinMemAlloc = 4096;
+        MainWindowState = "AAAA/wAAAAD9AAAAAAAABEwAAAVJAAAABAAAAAQAAAAIAAAACPwAAAADAAAAAAAAAAEAAAAeAGkAbgBzAHQAYQBuAGMAZQBUAG8AbwBsAEIAYQByAwAAAAD/////AAAAAAAAAAAAAAACAAAAAQAAABYAbQBhAGkAbgBUAG8AbwBsAEIAYQByAQAAAAD/////AAAAAAAAAAAAAAADAAAAAQAAABYAbgBlAHcAcwBUAG8AbwBsAEIAYQByAAAAAAD/////AAAAAAAAAAA=";
+        StatusBarVisible = false;
+        ToolbarsLocked = true;
+      };
     };
   };
 }
