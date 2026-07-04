e4c9260872f0d568676bbfe5d6f400fc14661947
greyxp1 <greyxp999@gmail.com>
2026-07-04 04:14:54 -0400

    stopped lazygit from prompting to initialize a repo

--- modules/programs/dev/helix.nix
+++ modules/programs/dev/helix.nix
@@ -1,32 +1,38 @@
 {
-  flake.homeModules.helix = {lib, pkgs, ...}: {
-    home.packages = [ pkgs.lazygit ];
-    programs.helix = {
-      enable = true;
-      defaultEditor = true;
+  flake.homeModules.helix = {lib, ...}: {
+    programs = {
+      helix = {
+        enable = true;
+        defaultEditor = true;
 
-      themes.catppuccin_transparent = {
-        inherits = "catppuccin_mocha";
-        "ui.background" = {bg = "none";};
+        themes.catppuccin_transparent = {
+          inherits = "catppuccin_mocha";
+          "ui.background" = {bg = "none";};
+        };
+
+        settings = {
+          theme = lib.mkForce "catppuccin_transparent";
+
+          editor = {
+            cursor-shape.normal = "bar";
+            auto-format = true;
+            default-yank-register = "+";
+          };
+
+          keys.normal = {
+            "C-g" = [
+              ":new"
+              ":insert-output lazygit"
+              ":buffer-close!"
+              ":redraw"
+            ];
+          };
+        };
       };
 
-      settings = {
-        theme = lib.mkForce "catppuccin_transparent";
-
-        editor = {
-          cursor-shape.normal = "bar";
-          auto-format = true;
-          default-yank-register = "+";
-        };
-
-        keys.normal = {
-          "C-g" = [
-            ":new"
-            ":insert-output lazygit"
-            ":buffer-close!"
-            ":redraw"
-          ];
-        };
+      lazygit = {
+        enable = true;
+        settings.notARepository = "skip";
       };
     };
   };
