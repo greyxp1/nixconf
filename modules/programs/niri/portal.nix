12eb5875b59b2aa0a22dbc98fb19c76529b1cca8
greyxp1 <greyxp999@gmail.com>
2026-07-07 18:23:56 -0400

    fixed a bug

--- modules/programs/niri/portal.nix
+++ modules/programs/niri/portal.nix
@@ -11,9 +11,8 @@ in {
         postPatch = (old.postPatch or "")
         + ''
                   substituteInPlace main.go \
-                    --replace-fail 'sessionObj.Call(mutterSessionInterface+".RecordWindow", 0,
-          windowOptions).Store(&streamPath)' 'sessionObj.Call(mutterSessionInterface+".RecordMonitor", 0,
-          "${output}", windowOptions).Store(&streamPath)' \
+                    --replace-fail 'sessionObj.Call(mutterSessionInterface+".RecordWindow", 0,' 'sessionObj.Call(mutterSessionInterface+".RecordMonitor", 0,' \
+                    --replace-fail 'windowOptions).Store(&streamPath)' '"${output}", windowOptions).Store(&streamPath)' \
                     --replace-fail '"position":    dbus.MakeVariant(map[string]int32{"x": 0, "y": 0}),' '"position":    dbus.MakeVariant(struct{ X, Y int32 }{0, 0}),' \
                     --replace-fail '"size":        dbus.MakeVariant(map[string]int32{"width": 0, "height": 0}),' '"size":        dbus.MakeVariant(struct{ Width, Height int32 }{${toString width}, ${toString height}}),' \
                     --replace-fail '"source_type": dbus.MakeVariant(uint32(2)), // WINDOW = 2' '"source_type": dbus.MakeVariant(uint32(1)), // MONITOR = 1'
