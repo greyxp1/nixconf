840e343746bd2c351ed9014fc2e92745b24976b4
greyxp1 <greyxp999@gmail.com>
2026-07-07 15:27:55 -0400

    formatted

--- modules/programs/ghostty/ghostty.nix
+++ modules/programs/ghostty/ghostty.nix
@@ -1,32 +1,29 @@
-{...}: {
-  flake.nixosModules.ghostty = {...}: {
-    home-manager.users.grey = {...}: {
-      programs.ghostty = {
-        enable = true;
-        settings = {
-          cursor-style = "bar";
-          background-opacity = 0.81;
-          font-size = 14;
-          window-padding-x = 8;
-          window-padding-y = 8;
-          window-padding-color = "extend";
-          cursor-style-blink = false;
-          confirm-close-surface = false;
-          link-url = true;
-          link-previews = true;
-          clipboard-trim-trailing-spaces = true;
-          copy-on-select = "clipboard";
-          selection-background = "#0078D7";
-          custom-shader-animation = "always";
-          custom-shader = ["${./cursor.glsl}"];
-          keybind = [
-            "ctrl+alt+h=goto_split:left"
-            "ctrl+alt+j=goto_split:down"
-            "ctrl+alt+k=goto_split:up"
-            "ctrl+alt+l=goto_split:right"
-            "ctrl+alt+y=select_all"
-          ];
-        };
+{
+  flake.homeModules.ghostty = {
+    programs.ghostty = {
+      enable = true;
+      settings = {
+        cursor-style = "bar";
+        background-opacity = 0.81;
+        font-size = 14;
+        window-padding-x = 8;
+        window-padding-y = 8;
+        window-padding-color = "extend";
+        cursor-style-blink = false;
+        confirm-close-surface = false;
+        link-url = true;
+        link-previews = true;
+        clipboard-trim-trailing-spaces = true;
+        copy-on-select = "clipboard";
+        custom-shader-animation = "always";
+        custom-shader = ["${./cursor.glsl}"];
+        keybind = [
+          "ctrl+alt+h=goto_split:left"
+          "ctrl+alt+j=goto_split:down"
+          "ctrl+alt+k=goto_split:up"
+          "ctrl+alt+l=goto_split:right"
+          "ctrl+alt+y=select_all"
+        ];
       };
     };
   };
