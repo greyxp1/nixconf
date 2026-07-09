bf89605dff707c3de8a79c3433f03ffd68ceece3
greyxp1 <greyxp999@gmail.com>
2026-07-09 14:30:55 -0400

    reorganize

--- modules/system/hosts/desktop/configuration.nix
+++ modules/system/hosts/desktop/configuration.nix
@@ -16,24 +16,24 @@
       # AMD CPU
       powerManagement.cpuFreqGovernor = "performance";
       hardware.cpu.amd.updateMicrocode = true;
-      boot.kernelParams = [
-        "amd_pstate=active" # AMD CPU freq scaling driver
-        "8250.nr_uarts=0" # suppress legacy COM port probes
-        "nowatchdog" # disable hardware watchdog drivers
-        "nmi_watchdog=0" # disable NMI watchdog
-      ];
-
       boot = {
         kernelModules = ["kvm-amd"];
-        loader.systemd-boot.extraEntries."windows.conf" = ''
-          title Windows Boot Manager
-          efi /EFI/Microsoft/Boot/bootmgfw.efi
-        '';
+        kernelParams = [
+          "amd_pstate=active" # AMD CPU freq scaling driver
+          "8250.nr_uarts=0" # suppress legacy COM port probes
+          "nowatchdog" # disable hardware watchdog drivers
+          "nmi_watchdog=0" # disable NMI watchdog
+        ];
 
         initrd = {
           systemd.network.wait-online.enable = false;
           availableKernelModules = ["nvme"];
         };
+
+        loader.systemd-boot.extraEntries."windows.conf" = ''
+          title Windows Boot Manager
+          efi /EFI/Microsoft/Boot/bootmgfw.efi
+        '';
       };
     };
   };
