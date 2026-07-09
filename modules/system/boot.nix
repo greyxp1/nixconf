f0806bfafbc3df6fcc72babfed8950eb9d279380
greyxp1 <greyxp999@gmail.com>
2026-07-09 14:31:04 -0400

    removed useless settigns

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -2,24 +2,14 @@
   flake.nixosModules.boot = {
     boot = {
       supportedFilesystems = ["btrfs"];
-      initrd = {
-        supportedFilesystems = ["btrfs"];
-        verbose = false;
-      };
-
+      initrd.supportedFilesystems = ["btrfs"];
+      kernelParams = ["systemd.show_status=auto"];
+      consoleLogLevel = 3;
       loader = {
         efi.canTouchEfiVariables = true;
         systemd-boot.enable = true;
         timeout = 0;
       };
-
-      consoleLogLevel = 3;
-      kernelParams = [
-        "quiet"
-        "udev.log_level=3"
-        "systemd.show_status=auto"
-        "vt.global_cursor_default=0"
-      ];
     };
   };
 }
