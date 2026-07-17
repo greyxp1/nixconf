585c6ce69181d26e21570a85ab9f4c4f0812150c
greyxp1 <greyxp999@gmail.com>
2026-07-17 18:04:04 -0400

    switched to using a single output for both the theme and icons

--- modules/programs/prism.nix
+++ modules/programs/prism.nix
@@ -1,29 +1,15 @@
-{
-  flake.homeModules.prism = {pkgs, ...}: let
+{inputs, ...}: {
+  flake.homeModules.prism = _: let
     themeName = "Fluent-Dark";
-
-    fluentDarkTheme = pkgs.fetchFromGitHub {
-      owner = "M3CHR0M4NC3R";
-      repo = "Prism-Launcher-Themes";
-      rev = "main";
-      sha256 = "1r0nbcyj3yna4jh5qvflxmg25xcymilp00iw0adq6n54774bsz84";
-    };
-
-    fluentDarkIcons = pkgs.fetchFromGitHub {
-      owner = "PrismLauncher";
-      repo = "Themes";
-      rev = "main";
-      sha256 = "0gzkdzvkxz0s915dsj975l0b1xxk57vjm5jc1g9g80wmshia9aap";
-    };
   in {
     xdg.dataFile."PrismLauncher/iconthemes/${themeName}" = {
-      source = "${fluentDarkIcons}/icons/${themeName}";
+      source = "${inputs.prism-themes}/icons/${themeName}";
       recursive = true;
     };
 
     programs.prismlauncher = {
       enable = true;
-      themes.${themeName} = "${fluentDarkTheme}/themes/${themeName}";
+      themes.${themeName} = "${inputs.prism-themes}/themes/${themeName}";
 
       settings = {
         Language = "en_US";
