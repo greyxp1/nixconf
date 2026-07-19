1ff7b3b874894ff08605ddfb903b7ab40e7732a0
greyxp1 <greyxp999@gmail.com>
2026-07-19 18:22:13 -0400

    fixed conflict with new upstream commit

--- modules/programs/niri/portal.nix
+++ modules/programs/niri/portal.nix
@@ -10,12 +10,11 @@ in {
       package = inputs.niri-autoselect-portal.packages.${pkgs.stdenv.hostPlatform.system}.default.overrideAttrs (old: {
         postPatch = (old.postPatch or "")
         + ''
-                  substituteInPlace main.go \
-                    --replace-fail 'sessionObj.Call(mutterSessionInterface+".RecordWindow", 0,' 'sessionObj.Call(mutterSessionInterface+".RecordMonitor", 0,' \
-                    --replace-fail 'windowOptions).Store(&streamPath)' '"${output}", windowOptions).Store(&streamPath)' \
-                    --replace-fail '"position":    dbus.MakeVariant(map[string]int32{"x": 0, "y": 0}),' '"position":    dbus.MakeVariant(struct{ X, Y int32 }{0, 0}),' \
-                    --replace-fail '"size":        dbus.MakeVariant(map[string]int32{"width": 0, "height": 0}),' '"size":        dbus.MakeVariant(struct{ Width, Height int32 }{${toString width}, ${toString height}}),' \
-                    --replace-fail '"source_type": dbus.MakeVariant(uint32(2)), // WINDOW = 2' '"source_type": dbus.MakeVariant(uint32(1)), // MONITOR = 1'
+          substituteInPlace main.go \
+            --replace-fail 'sessionObj.Call(mutterSessionInterface+".RecordWindow", 0,' 'sessionObj.Call(mutterSessionInterface+".RecordMonitor", 0,' \
+            --replace-fail 'windowOptions).Store(&streamPath)' '"${output}", windowOptions).Store(&streamPath)' \
+            --replace-fail '"size":        dbus.MakeVariant(size{0, 0}),' '"size":        dbus.MakeVariant(size{${toString width}, ${toString height}}),' \
+            --replace-fail '"source_type": dbus.MakeVariant(uint32(2)), // WINDOW = 2' '"source_type": dbus.MakeVariant(uint32(1)), // MONITOR = 1'
         '';
       });
     };
