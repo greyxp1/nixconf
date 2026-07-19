ffb9a743b559c2ad859c31f80fe61cb8ff9ebc94
greyxp1 <greyxp999@gmail.com>
2026-07-19 19:33:31 -0400

    removed musnix

--- modules/system/audio.nix
+++ modules/system/audio.nix
@@ -1,14 +1,6 @@
-{inputs, ...}: {
-  flake.nixosModules.audio = {...}: {
-    imports = [inputs.musnix.nixosModules.musnix];
-    users.users.grey.extraGroups = ["audio"];
+{
+  flake.nixosModules.audio = {
     security.rtkit.enable = true;
-    musnix.enable = true;
-    musnix.rtirq = {
-      enable = true;
-      nameList = "usb";
-    };
-
     services.pipewire = {
       enable = true;
       pulse.enable = true;
@@ -16,20 +8,6 @@
         enable = true;
         support32Bit = true;
       };
-
-      extraConfig.pipewire."99-lowlatency"."context.properties" = {
-        "default.clock.rate" = 48000;
-        "default.clock.quantum" = 128;
-        "default.clock.min-quantum" = 64;
-        "default.clock.max-quantum" = 512;
-      };
-
-      wireplumber.extraConfig."10-disable-hw-volume"."monitor.alsa.rules" = [
-        {
-          matches = [{"device.name" = "~alsa_card.*";}];
-          actions.update-props."api.alsa.soft-mixer" = true;
-        }
-      ];
     };
   };
 }
