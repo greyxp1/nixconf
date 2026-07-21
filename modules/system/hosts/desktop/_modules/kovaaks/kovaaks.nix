349f2e748798ee0ceba9aabdacabe0c1d2ed2bd9
greyxp1 <greyxp999@gmail.com>
2026-07-21 16:23:02 -0400

    bunch of changes

--- modules/system/hosts/desktop/_modules/kovaaks/kovaaks.nix
+++ modules/system/hosts/desktop/_modules/kovaaks/kovaaks.nix
@@ -1,4 +1,4 @@
-_: {
+{
   home-manager.users.grey = {config, lib, ...}: let
     src = ./config;
     dst = "${config.home.homeDirectory}/.local/share/Steam/steamapps/common/FPSAimTrainer/FPSAimTrainer";
@@ -11,13 +11,18 @@ _: {
           "${save}/Themes" \
           "${dst}/PrimaryUserSettings.json" \
           "${dst}/weaponsettings.ini" \
+          "${dst}/UI.json" \
           "${dst}/.nixconf-stamp"
         $DRY_RUN_CMD install -dm755 \
           "${dst}/crosshairs" \
           "${save}/Themes"
         $DRY_RUN_CMD install -m644 -t "${save}" \
           "${src}/PrimaryUserSettings.json" \
-          "${src}/weaponsettings.ini"
+          "${src}/weaponsettings.ini" \
+          "${src}/UI.json"
+        $DRY_RUN_CMD install -Dm644 \
+          "${src}/sounds/rxSound22.ogg" \
+          "${dst}/sounds/rxSound22.ogg"
         $DRY_RUN_CMD install -m644 -t "${dst}/crosshairs" "${src}/crosshairs/"*
         $DRY_RUN_CMD install -m644 -t "${save}/Themes" "${src}/Themes/"*
       fi
