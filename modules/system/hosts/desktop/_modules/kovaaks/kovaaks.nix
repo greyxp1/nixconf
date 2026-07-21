57067703f2e3edddd715d17aca190a888f4765f2
greyxp1 <greyxp999@gmail.com>
2026-07-21 08:40:08 -0400

    fixed a directory issue

--- modules/system/hosts/desktop/_modules/kovaaks/kovaaks.nix
+++ modules/system/hosts/desktop/_modules/kovaaks/kovaaks.nix
@@ -2,21 +2,24 @@ _: {
   home-manager.users.grey = {config, lib, ...}: let
     src = ./config;
     dst = "${config.home.homeDirectory}/.local/share/Steam/steamapps/common/FPSAimTrainer/FPSAimTrainer";
+    save = "${dst}/Saved/SaveGames";
   in {
     home.activation.kovaaks = lib.hm.dag.entryAfter ["writeBoundary"] ''
       if [ -d "${dst}" ]; then
         $DRY_RUN_CMD rm -rf \
           "${dst}/crosshairs" \
-          "${dst}/Saved/SaveGames/Themes" \
+          "${save}/Themes" \
+          "${dst}/PrimaryUserSettings.json" \
+          "${dst}/weaponsettings.ini" \
           "${dst}/.nixconf-stamp"
         $DRY_RUN_CMD install -dm755 \
           "${dst}/crosshairs" \
-          "${dst}/Saved/SaveGames/Themes"
-        $DRY_RUN_CMD install -m644 -t "${dst}" \
+          "${save}/Themes"
+        $DRY_RUN_CMD install -m644 -t "${save}" \
           "${src}/PrimaryUserSettings.json" \
           "${src}/weaponsettings.ini"
         $DRY_RUN_CMD install -m644 -t "${dst}/crosshairs" "${src}/crosshairs/"*
-        $DRY_RUN_CMD install -m644 -t "${dst}/Saved/SaveGames/Themes" "${src}/Themes/"*
+        $DRY_RUN_CMD install -m644 -t "${save}/Themes" "${src}/Themes/"*
       fi
     '';
   };
