1c9f53ba11296a019a343e4d10cd6ef3b84a42cb
greyxp1 <greyxp999@gmail.com>
2026-07-21 06:49:50 -0400

    test changes

--- modules/system/hosts/desktop/_modules/nvidia.nix
+++ modules/system/hosts/desktop/_modules/nvidia.nix
@@ -1,24 +1,17 @@
 {lib, pkgs, ...}: {
   environment.systemPackages = [pkgs.nvidia-vaapi-driver];
-
-  boot.kernelParams = [
-    "nvidia_drm.modeset=1"
-    "nvidia.NVreg_RegistryDwords=PerfLevelSrc=0x2222"
-  ];
-
-  services = {
-    xserver.videoDrivers = ["nvidia"];
-    acpid.enable = lib.mkForce false;
-  };
-
   hardware.nvidia = {
     open = true;
     modesetting.enable = true;
     nvidiaSettings = false;
-    nvidiaPersistenced = true;
     powerManagement = {
       enable = true;
       finegrained = false;
     };
+  };
+
+  services = {
+    xserver.videoDrivers = ["nvidia"];
+    acpid.enable = lib.mkForce false;
   };
 }
