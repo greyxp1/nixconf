b7ff7869865e662f592851b50680be2edf360f01
greyxp1 <greyxp999@gmail.com>
2026-07-21 15:36:11 -0400

    Revert "deleted kovaaks"
    This reverts commit 0a052d9c3144a02ad3b16bc3dbb8ef5c4fb854dd.

---
+++ modules/system/hosts/desktop/_modules/kovaaks/kovaaks.nix
@@ -0,0 +1,26 @@
+_: {
+  home-manager.users.grey = {config, lib, ...}: let
+    src = ./config;
+    dst = "${config.home.homeDirectory}/.local/share/Steam/steamapps/common/FPSAimTrainer/FPSAimTrainer";
+    save = "${dst}/Saved/SaveGames";
+  in {
+    home.activation.kovaaks = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      if [ -d "${dst}" ]; then
+        $DRY_RUN_CMD rm -rf \
+          "${dst}/crosshairs" \
+          "${save}/Themes" \
+          "${dst}/PrimaryUserSettings.json" \
+          "${dst}/weaponsettings.ini" \
+          "${dst}/.nixconf-stamp"
+        $DRY_RUN_CMD install -dm755 \
+          "${dst}/crosshairs" \
+          "${save}/Themes"
+        $DRY_RUN_CMD install -m644 -t "${save}" \
+          "${src}/PrimaryUserSettings.json" \
+          "${src}/weaponsettings.ini"
+        $DRY_RUN_CMD install -m644 -t "${dst}/crosshairs" "${src}/crosshairs/"*
+        $DRY_RUN_CMD install -m644 -t "${save}/Themes" "${src}/Themes/"*
+      fi
+    '';
+  };
+}
