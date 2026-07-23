c70372bca409f09952ce2ee2ecbc83c32fb976f4
greyxp1 <greyxp999@gmail.com>
2026-07-23 17:57:18 -0400

    now lets hosts import modules with inputs

--- modules/system/hosts/_mkHost.nix
+++ modules/system/hosts/_mkHost.nix
@@ -1,6 +1,7 @@
 inputs: hostName: hostModule:
 inputs.nixpkgs.lib.nixosSystem {
   system = "x86_64-linux";
+  specialArgs = {inherit inputs;};
   modules = [
     {networking.hostName = hostName;}
     hostModule
