28ddd14873c0a974ff1f115fa7c46f7188fa022c
greyxp1 <greyxp999@gmail.com>
2026-07-24 19:31:13 -0400

    moved niri steam settings into steam.nix and added vrr on demand as well as fullscreen rule

--- modules/system/hosts/desktop/_modules/steam.nix
+++ modules/system/hosts/desktop/_modules/steam.nix
@@ -1,5 +1,32 @@
 {pkgs, ...}: {
-  home-manager.users.grey.programs.mangohud.enable = true;
+  home-manager.sharedModules = [
+    {
+      programs.mangohud.enable = true;
+
+      wayland.windowManager.niri.settings = {
+        spawn-at-startup = [{_args = ["steam" "-silent"];}];
+        output = [
+          {
+            _args = ["DP-2"];
+            mode = "2560x1440@170.071";
+            variable-refresh-rate._props.on-demand = true;
+          }
+        ];
+        window-rule = [
+          {
+            match._props."app-id" = "^steam_app_";
+            open-fullscreen = true;
+            variable-refresh-rate = true;
+          }
+          {
+            match._props."app-id" = "^steam$";
+            open-fullscreen = false;
+          }
+        ];
+      };
+    }
+  ];
+
   programs.steam = {
     enable = true;
     extraCompatPackages = [pkgs.proton-ge-bin];
