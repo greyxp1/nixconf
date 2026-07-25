88e3281e9fe73ed774a2d3ce2d54abc80da7439f
greyxp1 <greyxp999@gmail.com>
2026-07-25 19:18:55 -0400

    updated lockscreen widgets

--- modules/system/hosts/desktop/_modules/noctalia.nix
+++ modules/system/hosts/desktop/_modules/noctalia.nix
@@ -12,23 +12,16 @@
 
     lockscreen_widgets = {
       enabled = true;
-      widget_order = [
-        "lockscreen-widget-0000000000000001"
-        "lockscreen-login-box@DP-2"
-        "lockscreen-widget-0000000000000003"
-      ];
       widget = {
         "lockscreen-login-box@DP-2" = {
-          box_height = 64.0;
-          box_width = 240.0;
-          cx = 1280.0;
-          cy = 768.0;
           output = "DP-2";
           type = "login_box";
           settings = {
-            background_radius = 32.0;
-            input_opacity = 1.0;
-            input_radius = 32.0;
+            background_opacity = 0.5;
+            background_radius = 15.0;
+            input_opacity = 0.75;
+            input_radius = 15.0;
+            center_password_text = true;
             show_caps_lock = false;
             show_keyboard_layout = false;
             show_login_button = false;
@@ -46,17 +39,6 @@
           settings = {
             background = false;
             center_text = true;
-          };
-        };
-
-        lockscreen-widget-0000000000000003 = {
-          cx = 1280.0;
-          cy = 1360.0;
-          output = "DP-2";
-          type = "audio_visualizer";
-          settings = {
-            background = false;
-            show_when_idle = false;
           };
         };
       };
