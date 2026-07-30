92fdcd21d77a7e3c6369ebd5627044606ae3a7ce
greyxp1 <greyxp999@gmail.com>
2026-07-29 22:21:30 -0400

    removed defaults

--- modules/programs/helium.nix
+++ modules/programs/helium.nix
@@ -7,13 +7,23 @@
       flags = ["--enable-features=HeliumMiddleClickAutoscroll"];
 
       extraPolicies = {
-        RestoreOnStartup = 1;
-        HighEfficiencyModeEnabled = true;
-        MemorySaverModeSavings = 2;
         DefaultSearchProviderEnabled = true;
         DefaultSearchProviderName = "Google";
         DefaultSearchProviderSearchURL = "https://www.google.com/search?q={searchTerms}";
         DefaultSearchProviderSuggestURL = "https://www.google.com/complete/search?output=chrome&q={searchTerms}";
+      };
+
+      preferences = {
+        helium.browser = {
+          show_back_button = false;
+          show_reload_button = false;
+          vertical_right_aligned = true;
+        };
+
+        browser = {
+          show_forward_button = false;
+          custom_chrome_frame = false;
+        };
       };
 
       extensions = {
@@ -40,31 +50,6 @@
         pipView = {
           id = "eaeedemddlledlghhjebjgdmhjekgegd";
           pin = true;
-        };
-      };
-
-      preferences = {
-        ntp.shortcust_visible = false;
-        auto_pin_new_tab_groups = false;
-        bookmark_bar.show_tab_groups = false;
-
-        helium = {
-          services.user_consented = true;
-          browser = {
-            layout = 2;
-            mru_tab_cycling = true;
-            show_avatar_button = false;
-            show_back_button = false;
-            show_reload_button = false;
-            rounded_frame = false;
-            show_vertical_tabs_collapse_button = false;
-            vertical_right_aligned = true;
-          };
-        };
-
-        browser = {
-          show_forward_button = false;
-          custom_chrome_frame = false;
         };
       };
     };
