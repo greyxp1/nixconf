9db1bac25f55ea74d2e4947883b6af158afa2871
greyxp1 <greyxp999@gmail.com>
2026-08-01 21:42:05 -0400

    updated steam module

--- modules/system/hosts/desktop/_modules/steam.nix
+++ modules/system/hosts/desktop/_modules/steam.nix
@@ -1,31 +1,20 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: {
-  imports = [inputs.chaotic.nixosModules.default];
+{pkgs, ...}: {
   programs.steam = {
     enable = true;
-    extraCompatPackages = [pkgs.proton-cachyos];
+    extraCompatPackages = [pkgs.proton-ge-bin];
     package = pkgs.steam.override {
       extraArgs = "-silent";
       extraEnv = {
         #STEAM_ENABLE_SHADER_CACHE_MANAGEMENT = "0";
         PROTON_ENABLE_WAYLAND = "1";
-        # DirectX 9–11
-        PROTON_DXVK_LOWLATENCY = "1";
-        DXVK_FRAME_RATE = "160";
-        DXVK_FRAME_PACE = "low-latency-vrr-167";
-        # DirectX 12
-        PROTON_VKD3D_LOWLATENCY = "1";
-        VKD3D_FRAME_RATE = "160";
+        DXVK_CONFIG = "dxvk.latencySleep = True; dxgi.maxFrameRate = 167; d3d9.maxFrameRate = 167";
+        VKD3D_FRAME_RATE = "167";
       };
     };
   };
 
   home-manager.sharedModules = [
     {
-      programs.mangohud.enable = true;
       wayland.windowManager.niri.settings._children = [
         {
           output = {
