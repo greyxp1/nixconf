2ebdb20804e3672ee739e38fafff15602ca17539
greyxp1 <greyxp999@gmail.com>
2026-08-04 12:10:53 -0400

    replaced swash with chameleos

--- modules/programs/niri/screenshots.nix
+++ modules/programs/niri/screenshots.nix
@@ -1,6 +1,5 @@
 {inputs, ...}: {
   flake.homeModules.niri-screenshots = {pkgs, ...}: let
-    swash = inputs.swash.packages.${pkgs.stdenv.hostPlatform.system}.default;
     tesseract = pkgs.tesseract.override {enableLanguages = ["eng"];};
     capture = command: bind "niri msg screenshot --stdout | ${command}";
     bind = spawn-sh: {
@@ -8,15 +7,20 @@
       inherit spawn-sh;
     };
   in {
-    imports = [inputs.perch.homeModules.default];
+    imports = [
+      inputs.chameleos.homeModules.default
+      inputs.perch.homeModules.default
+    ];
+
+    services.chameleos.enable = true;
     programs.perch.enable = true;
-    home.packages = [swash tesseract pkgs.wl-clipboard];
+    home.packages = [tesseract pkgs.wl-clipboard];
     wayland.windowManager.niri.settings = {
       binds = {
         "Mod+Shift+C" = bind "niri msg pick-color | wl-copy";
         "Shift+Print" = capture "perch -";
-        "Alt+Print" = capture "swash";
         "Ctrl+Print" = capture "tesseract stdin stdout | wl-copy";
+        "Alt+Print" = bind "chamel toggle";
       };
 
       _children = [
@@ -30,12 +34,6 @@
               active-color = "#cba6f7";
               inactive-color = "#cba6f7";
             };
-          };
-        }
-        {
-          window-rule = {
-            match._props."app-id" = "^dev\\.lemmy\\.swash$";
-            open-floating = true;
           };
         }
       ];
