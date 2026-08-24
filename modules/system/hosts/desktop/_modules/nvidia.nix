ebe65b443ae29b66d3423d39616bf3da6c98f852
greyxp1 <greyxp999@gmail.com>
2026-08-24 19:28:06 -0400

    selected latest nvidia driver

--- modules/system/hosts/desktop/_modules/nvidia.nix
+++ modules/system/hosts/desktop/_modules/nvidia.nix
@@ -6,6 +6,7 @@
   environment.systemPackages = [pkgs.nvidia-vaapi-driver];
   boot.kernelParams = ["nvidia.NVreg_RegistryDwords=PerfLevelSrc=0x2222"];
   hardware.nvidia = {
+    branch = "latest";
     open = true;
     modesetting.enable = true;
     nvidiaSettings = false;
